c1c2e8ae278c31b9039e764c35300155e9fc700e
SFJT-97 <santiagojuarez.dev@gmail.com>
2024-03-21 09:49:25 -0400

    Array!

---
+++ Array/array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main()
+{
+    char hello[50] = " ";
+    char im[50] = " ";
+    char getting[50] = " ";
+
+    strcat(hello, "Hello "); /* store world in hello */
+    strcat(im, "I'm "); /* store just in im */
+    strcat(getting, "getting "); /* store started in getting */
+ 
+    char world[] = "World!";
+    char just[] = "just";
+    char started[] = "started.";
+
+    strcat(hello, world); /* store world in hello */
+    strcat(im, just); /* store just in im */
+    strcat(getting, started); /* store started in getting */
+
+    strcat(hello, im); /* store new im in new hello */
+    
+    strcat(hello, getting); /* store new getting in new new hello */
+
+    printf("%s", hello );
+
+    return 0;
+}
